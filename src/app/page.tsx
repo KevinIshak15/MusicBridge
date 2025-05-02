410283c99ec6f55ea756105014e8cbb758b469fb
KevinIshak15 <56769270+KevinIshak15@users.noreply.github.com>
2025-05-02 18:37:33 -0400

    Initial cleaned setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black-100 text-3xl font-bold">
+      Welcome to MusicBridge 🎵
+    </main>
+  );
+}
